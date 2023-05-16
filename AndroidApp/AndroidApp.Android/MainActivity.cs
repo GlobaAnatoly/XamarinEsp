aa63aa31d5f45f6d53634e0d7ad02df15ff9136b
Ihor_Mukhortov <igorleo528@gmail.com>
2023-05-17 02:05:42 +0300

    Fixed permission issue

--- AndroidApp/AndroidApp.Android/MainActivity.cs
+++ AndroidApp/AndroidApp.Android/MainActivity.cs
@@ -4,19 +4,23 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using AndroidApp.Droid.Permission;
+using Xamarin.Essentials;
 
 namespace AndroidApp.Droid
 {
     [Activity(Label = "AndroidApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            await Permissions.RequestAsync<BLEPermission>();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
